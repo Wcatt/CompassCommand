6071c1f1c0cb7e58268aedaaec9fcff854399d42
Danburen <2964361672@qq.com>
2024-09-18 22:10:58 +0800

    rebuild project

---
+++ src/main/java/waterfun/waterwood/compasscommand/CompassCommand.java
@@ -0,0 +1,44 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package waterfun.waterwood.compasscommand;
+
+import org.bukkit.Bukkit;
+import me.waterwood.BukkitPlugin;
+
+
+public final class CompassCommand extends BukkitPlugin {
+    private static boolean hasPapi = false;
+    protected static CompassCommand instance;
+    public void onEnable() {
+        this.initialization();
+        instance = this;
+        this.showPluginTitle("CPCCMD");
+        this.loadConfig(false);
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
+            logMsg(getPluginMessage("papi-enabled-message"));
+            hasPapi  = true;
+        }
+        logMsg(getPluginMessage("config-load-message"));
+        this.checkUpdate("Wcatt","CompassCommand");
+        this.getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
+        getCommand("CompassCommand").setExecutor(new Commands(this));
+        getCommand("CompassCommand").setTabCompleter(new Commands(this));
+
+    }
+
+    public void onDisable() {
+
+    }
+
+    public static CompassCommand getInstance() {
+        return instance;
+    }
+
+    public static boolean hasPapi(){
+        return hasPapi;
+    }
+
+}
